3d0e27e957df7333e47fcb899b3c246c8b9be075
rodrigoo1604 <rodrigo.lastra.pescador@gmail.com>
2024-11-28 15:11:57 +0100

    feat: created front to show the database

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,27 @@
-<div>
-    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
+@extends('layouts.app')
+
+@section('content')
+@section('content')
+<div class="tableApplication">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Date</th>
+                <th scope="col">Company Name</th>
+                <th scope="col">Offer Details</th>
+                <th scope="col">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($applications as $application)
+                <tr>
+                    <td>{{ $application->created_at}}</td>
+                    <td>{{ $application->companyName}}</td>
+                    <td>{{ $application->details}}</td>
+                    <td>{{ $application->finalised ? 'Finalised' : 'In Progress' }}</td>
+                </tr>
+            @endforeach
+        <tr>
+    </table>
 </div>
+@endsection
