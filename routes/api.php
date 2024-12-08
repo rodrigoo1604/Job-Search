a173ab7eeca68792359d1e083c14ef84832930da
rodrigoo1604 <rodrigo.lastra.pescador@gmail.com>
2024-12-08 16:56:26 +0100

    feat: added all api routes

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApplicationController;
 
-Route::get('/applications',[ApplicationController::class,'index'])->name('apihome');
+Route::get('/applications', [ApplicationController::class, 'index'])->name('apihome');
+Route::delete('/applications/{id}', [ApplicationController::class, 'destroy'])->name('apidestroy');
+Route::post('/applications', [ApplicationController::class, 'store'])->name('apistore');
+Route::put('/applications/{id}', [ApplicationController::class, 'update'])->name('apiupdate');
+Route::get('/applications/{id}',[ApplicationController::class, 'show'])->name('apishow');
