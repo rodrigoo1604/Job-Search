3eeade4a251d8712db2910d3eb5716a74b6a6e0f
rodrigoo1604 <rodrigo.lastra.pescador@gmail.com>
2024-12-10 09:03:15 +0100

    feat: added styles

---
+++ resources/views/Components/header.blade.php
@@ -0,0 +1,5 @@
+<header class="header">
+    <div class="header-content">
+        <h1>Job Search</h1>
+    </div>
+</header>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,17 +4,24 @@
         <h2 class="application->follows">Entry</h2>
         <div class="applicationTime">
             <table>
-                <th scope="col">Comments</th>
-                <th scope="col">Created</th>
-                <th scope="col">Updated</th>
-                @foreach ($application->follows as $news)
-                <tr>
-                    <td>{{$news->news}}</td>
-                    <td>{{$news->updated_at}}</td>
-                    <td>{{$news->created_at}}</td>
-                </tr>
-                @endforeach
+                <thead>
+                    <tr>
+                        <th scope="col">Created</th>
+                        <th scope="col">Updated</th>
+                        <th scope="col">Comments</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($application->follows as $news)
+                    <tr>
+                        <td>{{ $news->created_at }}</td>
+                        <td>{{ $news->updated_at }}</td>
+                        <td>{{ $news->news }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
             </table>
+            
         </div>
         <div class="applicationContent">
             <p class="applicationEntry">{{$application['entry']}}</p>
